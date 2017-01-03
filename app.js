768d9bd011a96b5425d37b99e65d2dc288ff0ff7
Min Min <minmhan@gmail.com>
2017-01-03 22:42:05 +0800

    add local-auth

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
+
+var passport = require('passport');
+
+var flash = require('connect-flash');
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var configDb = require('./config/db.js');
+mongoose.connect(configDb.url);
+
+require('./config/passport.js')(passport);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
 
 
 app.set('view engine', 'ejs');
+
+//require for passport
+app.use(session({secret:'ilovescotchscotchyscotchscotch'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// routes
+require('./app/routes.js')(app, passport);
 
 app.listen(port);
 console.log('app running on port ' + port);
