d91dcab19e06c31348ba54da620018c2b1ac6204
Min Min <minmhan@gmail.com>
2017-01-06 00:08:55 +0800

    update bootstrap path

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var path = require('path');
 
 var configDb = require('./config/db.js');
 mongoose.connect(configDb.url);
@@ -20,7 +21,7 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser());
 
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 //require for passport
